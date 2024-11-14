46e1932bca23e5152eef73fb49765a70a552259b
Yaser Ahmady <y@yaserahmady.com>
2024-11-14 13:27:23 +0100

    Init

---
+++ convert.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm -rf ./2000 ./2003
+archmage -x ./chm/rpg2000.chm "./2000"
+archmage -x ./chm/rpg2003.chm "./2003"
+
+find . -type f -name "*.htm" -exec sh -c '
+  f="{}"
+  iconv --from-code=ISO-8859-1 --to-code=UTF-8 -- "$f" > "$f".tmp
+  mv -v -- "$f".tmp "$f"
+  ' \;
